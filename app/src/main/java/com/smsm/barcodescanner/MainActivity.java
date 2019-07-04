2221379493284e61dcddab62fbef65300dd567f6
OsamaElkot <gaky_love88@yahoo.com>
2019-07-04 20:20:00 +0200

    not continued camera

--- app/src/main/java/com/smsm/barcodescanner/MainActivity.java
+++ app/src/main/java/com/smsm/barcodescanner/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     public void handleResult(Result result) {
         Toast.makeText(this, result.getText(), Toast.LENGTH_SHORT).show();
-        scannerview.resumeCameraPreview(this);
+       // scannerview.resumeCameraPreview(this);
     }
 
     @Override
